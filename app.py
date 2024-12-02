9855f0f41fa0f436998f8260e38b3b17fe01c640
Meny Kobel <132544324+sfc-gh-mkobel@users.noreply.github.com>
2024-12-02 09:04:47 +0200

    Update and rename streamlit.py to app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+
+# Write directly to the app
+st.title("Example Streamlit App :balloon:")
+st.write(
+    """Replace this example with your own code!
+    **And if you're new to Streamlit,** check
+    out our easy-to-follow guides at
+    [docs.streamlit.io](https://docs.streamlit.io).
+    """
+)
